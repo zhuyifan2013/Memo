15d8cd6b4dabd6d64b2e1ca62c2ea4d07d4387fa
zhuyifan <zhuyifan@xiaomi.com>
2014-09-18 21:13:44 +0800

    uncomplete, add swipelayout
    N/A
    
    Signed-off-by: zhuyifan <zhuyifan@xiaomi.com>

---
+++ app/src/main/java/our/memo/SwipeListview/SwipeLayout.java
@@ -0,0 +1,24 @@
+package our.memo.SwipeListview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by yifan on 14-9-18.
+ * Email: zhuyifan@xiaomi.com
+ */
+public class SwipeLayout extends FrameLayout{
+
+    public SwipeLayout(Context context) {
+        super(context);
+    }
+
+    public SwipeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SwipeLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
